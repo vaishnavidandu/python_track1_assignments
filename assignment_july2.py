8c1305d6378d7d27f8382c005cbd1fa1f8d57d62
vaishnavidandu <142648469+vaishnavidandu@users.noreply.github.com>
2025-07-02 19:04:10 +0530

    Add files via upload

---
+++ assignment_july2.py
@@ -0,0 +1,79 @@
+# 1 que----- variables
+#variables are used to store the values
+
+a=2
+b=9
+print(a+b)    #11
+
+# 2 que----subtract 2 num
+
+a=20
+b=15
+print(a-b)    #5
+
+# 3 que ----multiply 2 variables and store result in 3rd var
+
+a=2
+b=5
+c=(a*b)
+print(c)    #10
+
+# 4 que ---divide without decimal
+
+a=10
+b=3
+print(10//3)  #3
+
+# 5 que --- floor division
+a=17
+b=4
+print(a//b)    #4
+
+# 6 que --- modulo operator
+
+a=25
+b=6
+print(a%b)    #1
+
+# 7 que ---- square
+
+a=2
+b=2
+print(a**b)  #4
+
+# 8 que --- average
+ 
+a,b,c=2,3,4
+print((a+b+c)/3)  #3
+
+# 9 que --- cube
+
+a=2
+b=(a**a)
+print(b*a)   #8
+
+# 10 que --- area of rectangle
+
+length=10
+width=15
+print(length*width) # 150
+
+# 11 que --- percentage
+
+total_marks=450
+obtained_marks=375
+print((obtained_marks/total_marks)*100) #83.33%
+
+# 12 que --- (a+b)*c
+
+a,b,c=10,20,30
+print((a+b)*c)   #900
+
+# 13 que --- re assignment
+# if a=3 , b=4   then after if  we assign a=5 the value which is stored in memory block 
+# will remain same but the refrence of the value will be changed 
+# from a=3 the connection will cuttoff and it replaced to connect with a=5
+# which we assigned next time bcz line by line execution is done here
+
+
+
